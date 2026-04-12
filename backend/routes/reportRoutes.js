2a35c9f0b0bb28eee01dd0cc32ec2421fc682806
stupidteamz <stupidteamz@gmail.com>
2026-04-12 23:18:27 +0700

    Add Excel export and vehicle report features

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { generateTripReport, generatePaymentReport, getDashboardSummary } = require('../controllers/reportController');
+const { 
+    getDashboardSummary, 
+    generateTripReport, 
+    generatePaymentReport,
+    generateExcelReport,
+    generateVehicleReport
+} = require('../controllers/reportController');
 
+router.get('/summary', getDashboardSummary);
 router.get('/trips', generateTripReport);
 router.get('/payments', generatePaymentReport);
-router.get('/summary', getDashboardSummary);
+router.get('/excel', generateExcelReport);
+router.get('/vehicle', generateVehicleReport);
 
 module.exports = router;
